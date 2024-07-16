4400f0d1bfb6d3db04d75350c49931850120b94b
Jack Bullen <jackbullen@icloud.com>
2024-07-16 15:35:15 -0700

    hardcoded texture 05-Textures

--- 05-Textures/src/Renderer.m
+++ 05-Textures/src/Renderer.m
@@ -4,6 +4,14 @@
 {
     id<MTLDevice> _pDevice;
     id<MTLCommandQueue> _pCommandQueue;
+    id<MTLRenderPipelineState> _pPSO;
+    id<MTLBuffer> _pVertexDataBuffer;
+    id<MTLBuffer> _pIndexBuffer;
+    id<MTLBuffer> _pCameraDataBuffer;
+    id<MTLLibrary> _pShaderLibrary;
+    id<MTLDepthStencilState> _pDepthStencilState;
+    id<MTLTexture> _pTexture;
+    float _angle;
 }
 @end
 
@@ -16,22 +24,208 @@
     {
         _pDevice = device;
         _pCommandQueue = [_pDevice newCommandQueue];
+        [self buildShaders];
+        [self buildTextures];
+        [self buildBuffers];
     }
     return self;
 }
 
 - (void)dealloc 
 {
+    [_pDevice release];
+    [_pCommandQueue release];
+    [_pPSO release];
+    [_pShaderLibrary release];
+    [_pTexture release];
     [super dealloc];
+}
+
+- (void)buildShaders 
+{
+    NSError *error = nil;
+
+    NSString *shaderSrc = [NSString stringWithContentsOfFile:@"src/shaders/05-Textures.metal" 
+                                        encoding:NSUTF8StringEncoding 
+                                            error:&error];
+
+    _pShaderLibrary = [_pDevice newLibraryWithSource:shaderSrc options:nil error:&error];
+    if (!_pShaderLibrary)
+    {
+        NSLog(@"Could not create shader library: %@", error.localizedDescription);
+        return;
+    }
+
+    id<MTLFunction> pVertexFn = [_pShaderLibrary newFunctionWithName:@"vertexMain"];
+    if (!pVertexFn)
+    {
+        NSLog(@"Failed to load vertex function");
+        return;
+    }
+
+    id<MTLFunction> pFragFn = [_pShaderLibrary newFunctionWithName:@"fragmentMain"];
+    if (!pFragFn)
+    {
+        NSLog(@"Failed to load fragment function");
+        return;
+    }
+
+    MTLRenderPipelineDescriptor* pDesc = [[MTLRenderPipelineDescriptor alloc] init];
+    pDesc.vertexFunction = pVertexFn;
+    pDesc.fragmentFunction = pFragFn;
+    pDesc.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm_sRGB;
+    [pDesc setDepthAttachmentPixelFormat:MTLPixelFormatDepth16Unorm];
+
+    _pPSO = [_pDevice newRenderPipelineStateWithDescriptor:pDesc error:&error];
+    if (!_pPSO) 
+    {
+        NSLog(@"Failed to create render pipeline state: %@", error.localizedDescription);
+        return;
+    }
+}
+
+- (void)buildDepthStencilState 
+{
+    MTLDepthStencilDescriptor *pDsDesc = [[MTLDepthStencilDescriptor alloc] init];
+    pDsDesc.depthCompareFunction = MTLCompareFunctionLess;
+    pDsDesc.depthWriteEnabled = YES;
+
+    _pDepthStencilState = [_pDevice newDepthStencilStateWithDescriptor:pDsDesc];
+    [pDsDesc release];
+}
+
+- (void)buildTextures 
+{
+    const uint32_t tw = 128;
+    const uint32_t th = 128;
+
+    MTLTextureDescriptor* pTextureDesc = [[MTLTextureDescriptor alloc] init];
+    [pTextureDesc setWidth:tw];
+    [pTextureDesc setHeight:th];
+    [pTextureDesc setPixelFormat:MTLPixelFormatBGRA8Unorm];
+    [pTextureDesc setTextureType:MTLTextureType2D];
+    [pTextureDesc setStorageMode:MTLStorageModeManaged];
+    [pTextureDesc setUsage:MTLResourceUsageSample|MTLResourceUsageRead];
+
+    _pTexture = [_pDevice newTextureWithDescriptor:pTextureDesc];
+
+    uint8_t *pTextureData = (uint8_t*)alloca(tw*th*4);
+    for (size_t y = 0; y < th; y++)
+    {
+        for (size_t x = 0; x < tw; x++)
+        {
+            bool isWhite = (x^y) & 0b1000000;
+            uint8_t c = isWhite ? 0xFF : 0xA;
+
+            size_t i = (y * tw) + x;
+
+            pTextureData[i*4 + 0] = c;
+            pTextureData[i*4 + 1] = c;
+            pTextureData[i*4 + 2] = c;
+            pTextureData[i*4 + 3] = 0xFF;
+        }
+    }
+
+    [_pTexture replaceRegion:MTLRegionMake3D(0, 0, 0, tw, th, 1) mipmapLevel:0 withBytes:pTextureData bytesPerRow:tw*4];
+
+    [pTextureDesc release];
+}
+
+- (void)buildBuffers 
+{
+    const float s = 0.5f;
+
+    struct VertexData vertices[] = {
+        //                                         Texture
+        //   Positions           Normals         Coordinates
+        { { -s, -s, +s }, {  0.f,  0.f,  1.f }, { 0.f, 1.f } },
+        { { +s, -s, +s }, {  0.f,  0.f,  1.f }, { 1.f, 1.f } },
+        { { +s, +s, +s }, {  0.f,  0.f,  1.f }, { 1.f, 0.f } },
+        { { -s, +s, +s }, {  0.f,  0.f,  1.f }, { 0.f, 0.f } },
+
+        { { +s, -s, +s }, {  1.f,  0.f,  0.f }, { 0.f, 1.f } },
+        { { +s, -s, -s }, {  1.f,  0.f,  0.f }, { 1.f, 1.f } },
+        { { +s, +s, -s }, {  1.f,  0.f,  0.f }, { 1.f, 0.f } },
+        { { +s, +s, +s }, {  1.f,  0.f,  0.f }, { 0.f, 0.f } },
+
+        { { +s, -s, -s }, {  0.f,  0.f, -1.f }, { 0.f, 1.f } },
+        { { -s, -s, -s }, {  0.f,  0.f, -1.f }, { 1.f, 1.f } },
+        { { -s, +s, -s }, {  0.f,  0.f, -1.f }, { 1.f, 0.f } },
+        { { +s, +s, -s }, {  0.f,  0.f, -1.f }, { 0.f, 0.f } },
+
+        { { -s, -s, -s }, { -1.f,  0.f,  0.f }, { 0.f, 1.f } },
+        { { -s, -s, +s }, { -1.f,  0.f,  0.f }, { 1.f, 1.f } },
+        { { -s, +s, +s }, { -1.f,  0.f,  0.f }, { 1.f, 0.f } },
+        { { -s, +s, -s }, { -1.f,  0.f,  0.f }, { 0.f, 0.f } },
+
+        { { -s, +s, +s }, {  0.f,  1.f,  0.f }, { 0.f, 1.f } },
+        { { +s, +s, +s }, {  0.f,  1.f,  0.f }, { 1.f, 1.f } },
+        { { +s, +s, -s }, {  0.f,  1.f,  0.f }, { 1.f, 0.f } },
+        { { -s, +s, -s }, {  0.f,  1.f,  0.f }, { 0.f, 0.f } },
+
+        { { -s, -s, -s }, {  0.f, -1.f,  0.f }, { 0.f, 1.f } },
+        { { +s, -s, -s }, {  0.f, -1.f,  0.f }, { 1.f, 1.f } },
+        { { +s, -s, +s }, {  0.f, -1.f,  0.f }, { 1.f, 0.f } },
+        { { -s, -s, +s }, {  0.f, -1.f,  0.f }, { 0.f, 0.f } }
+    };
+
+    uint16_t indices[] = {
+         0,  1,  2,  2,  3,  0, /* front */
+         4,  5,  6,  6,  7,  4, /* right */
+         8,  9, 10, 10, 11,  8, /* back */
+        12, 13, 14, 14, 15, 12, /* left */
+        16, 17, 18, 18, 19, 16, /* top */
+        20, 21, 22, 22, 23, 20, /* bottom */
+    };
+
+    const size_t verticesDataSize = sizeof(vertices);
+    const size_t indicesDataSize = sizeof(indices);
+
+    _pVertexDataBuffer = [_pDevice newBufferWithBytes:vertices length:verticesDataSize options:MTLResourceStorageModeManaged];
+    [_pVertexDataBuffer didModifyRange:NSMakeRange(0, _pVertexDataBuffer.length)];
+
+    _pIndexBuffer = [_pDevice newBufferWithBytes:indices length:indicesDataSize options:MTLResourceStorageModeManaged];
+    [_pIndexBuffer didModifyRange:NSMakeRange(0, _pIndexBuffer.length)];
+
+    _pCameraDataBuffer = [_pDevice newBufferWithLength:sizeof(struct CameraData) options:MTLResourceStorageModeManaged];
 }
 
 - (void)draw:(MTKView *)pView
 {
     @autoreleasepool
     {
+        _angle += 0.01f;
+
+        simd_float3 pos = {0.f, 0.f, -5.f};
+
+        struct CameraData* pCameraData = [_pCameraDataBuffer contents];
+        pCameraData->transform = matrix_multiply(makeTranslate(pos), makeYRotate(_angle));
+        pCameraData->normalTransform = chopMat(pCameraData->transform);
+        pCameraData->perspective = makePerspective(45.f * M_PI / 180.f, 1.f, 0.03f, 500.0f);
+        pCameraData->world = makeIdentity();
+        pCameraData->worldNormal = chopMat(makeIdentity());
+
         id<MTLCommandBuffer> pCmd = [_pCommandQueue commandBuffer];
+
         MTLRenderPassDescriptor* pRpd = pView.currentRenderPassDescriptor;
         id<MTLRenderCommandEncoder> pEnc = [pCmd renderCommandEncoderWithDescriptor:pRpd];
+
+        [pEnc setRenderPipelineState:_pPSO];
+        [pEnc setDepthStencilState:_pDepthStencilState];
+        [pEnc setFragmentTexture:_pTexture atIndex:0];
+        [pEnc setCullMode:MTLCullModeBack];
+        [pEnc setFrontFacingWinding:MTLWindingCounterClockwise];
+
+        [pEnc setVertexBuffer:_pVertexDataBuffer offset:0 atIndex:0];
+        [pEnc setVertexBuffer:_pCameraDataBuffer offset:0 atIndex:1];
+
+        [pEnc drawIndexedPrimitives:MTLPrimitiveTypeTriangle 
+                        indexCount:36 
+                        indexType:MTLIndexTypeUInt16 
+                        indexBuffer:_pIndexBuffer 
+                        indexBufferOffset:0 
+                        instanceCount:1];
+
         [pEnc endEncoding];
         [pCmd presentDrawable:pView.currentDrawable];
         [pCmd commit];
---
+++ 05-Textures/src/shaders/05-Textures.metal
@@ -0,0 +1,61 @@
+#include <metal_stdlib>
+using namespace metal;
+
+struct v2f 
+{
+    float4 position [[position]];
+    float3 normal;
+    half4 color;
+    float2 texcoord;
+};
+
+struct VertexData
+{
+    float3 position;
+    float3 normal;
+    float2 texcoord;
+};
+
+struct CameraData
+{
+    float4x4 transform;
+    float3x3 normalTransform;
+    float4x4 perspective;
+    float4x4 world;
+    float3x3 worldNormal;
+};
+
+v2f vertex vertexMain(device const VertexData* vertexData [[buffer(0)]],
+                      device const CameraData& cameraData [[buffer(1)]],
+                      uint vid [[vertex_id]])
+{
+    v2f o;
+
+    const device VertexData& vd = vertexData[vid];
+    float4 pos = float4(vd.position, 1.0);
+    pos = cameraData.transform * pos;
+    pos = cameraData.perspective * cameraData.world * pos;
+    o.position = pos;
+
+    float3 normal = cameraData.normalTransform * vd.normal;
+    normal = cameraData.worldNormal * normal;
+    o.normal = normal;
+
+    o.texcoord = vd.texcoord;
+
+    o.color = half4(1.0, 1.0, 1.0, 1.0);
+    return o;
+}
+
+half4 fragment fragmentMain(v2f in [[stage_in]],
+                            texture2d<half, access::sample> tex [[texture(0)]])
+{
+    constexpr sampler s(address::repeat, filter::linear);
+    half3 texel = tex.sample(s, in.texcoord).rgb;
+
+    float3 lightD = normalize(float3(1.0, 1.0, 1.0));
+    float3 normal = normalize(in.normal);
+
+    half3 illumin = in.color.rgb * texel * saturate(dot(lightD, normal));
+    return half4(illumin, in.color.a);
+}
